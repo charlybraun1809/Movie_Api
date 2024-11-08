bf2ba0d79c421b1501cb85fc459e581ceb1e96f1
charlybraun1809 <charly.braun1809@gmail.com>
2024-11-08 08:04:41 +0100

    created dataIntoJsn functiono shorted init function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+
+let trendingMovies = [];
+let genres = [];
+let baseUrl = "https://api.themoviedb.org/3/";
+
 const options = {
     method: 'GET',
     headers: {
@@ -6,29 +12,64 @@ const options = {
     }
 };
 
-let trendingMovies = [];
-
-
-async function getData() {
-    await fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
-        .then(res => res.json())
-        .then(data => {
-            trendingMovies = data.results;
-             renderMovies(trendingMovies);
-             console.log(trendingMovies);
-        })
-        .catch(err => console.error(err));
+// Genre-Liste laden und zwischenspeichern
+async function fetchGenres() {
+    try {
+        let response = await fetch(`${baseUrl}genre/movie/list?language=en-US`, options);
+        let data = await response.json();
+        genres = data.genres; // Genres zwischenspeichern
+    } catch (error) {
+        console.error('Fehler beim Laden der Genres:', error);
+    }
 }
 
+// Genres-IDs in Genre-Namen umwandeln
+function getGenreNamesByIds(genreIds) {
+    return genreIds.map(id => {
+        let genre = genres.find(g => g.id === id);
+        return genre ? genre.name : "Unknown";
+    }).join(", ");
+}
+
+// Movies rendern
 function renderMovies(movies) {
     let contentRef = document.getElementById('content');
+    contentRef.innerHTML = ""; // Inhalt leeren
     movies.forEach(movie => {
+        let genreNames = getGenreNamesByIds(movie.genre_ids);
         contentRef.innerHTML += `
         <div id="movieCard">
-            <div class="cardHeader">${movie.title}</div>
-            <img class="poster" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title} poster"<img/>
-        </div>`
+            <div class="cardHeader">
+                <h2>${movie.title}</h2>
+                <p>Genres: ${genreNames}</p>
+            </div>
+            <img class="poster" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title} poster"/>
+            <div class="cardFooter"><b>${movie.overview}</b></div>
+        </div>`;
     });
 }
+
+// Daten abrufen und rendern
+async function init() {
+    await getData();
+    await dataIntoJson();
+    await fetchGenres();
+}
+
+async function dataIntoJson() {
+    let response = await getData('trending/movie/day?language=en-US');
+    trendingMovies = response.results;
+    renderMovies(trendingMovies);
+}
+
+async function getData(path = "") {
+    try {
+        let response = await fetch(baseUrl + path, options);
+        return response.json();
+    } catch (error) {
+        console.error('failed to fetch', error);
+    }
+}
+
 
 
